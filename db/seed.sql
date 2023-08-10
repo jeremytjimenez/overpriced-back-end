da21c222e9b3c5643027779a8041fff5eb167ffb
Irfan Hanjra <ihanjra@gmail.com>
2023-08-10 12:49:46 -0700

    Added more games to table

--- db/seed.sql
+++ db/seed.sql
@@ -5,4 +5,7 @@ INSERT INTO
 VALUES
     ('Silent Hill', 1999, 'Team Silent', 49.99, 177.94, 'Survival Horror', false, 'https://upload.wikimedia.org/wikipedia/en/9/96/Silent_Hill_video_game_cover.png'),
     ('Final Fantasy VII', 1997, 'Square', 49.99, 35.31, 'Turn Based RPG', false, 'https://upload.wikimedia.org/wikipedia/en/c/c2/Final_Fantasy_VII_Box_Art.jpg'),
-    ('Metal Gear Solid', 1998, 'Konami', 49.99, 43.00, 'Stealth', false, 'https://upload.wikimedia.org/wikipedia/en/3/33/Metal_Gear_Solid_cover_art.png');
+    ('Metal Gear Solid', 1998, 'Konami', 49.99, 43.00, 'Stealth', false, 'https://upload.wikimedia.org/wikipedia/en/3/33/Metal_Gear_Solid_cover_art.png'),
+    ('Breath of Fire 3', 1997, 'Capcom', 49.99, 63.71, 'RPG', false, 'https://upload.wikimedia.org/wikipedia/en/4/47/Breath_of_Fire_III_cover.png'),
+    ('Rule of Rose', 2006, 'Atlas', 49.99, 700.00, 'Survival Horror', false, 'https://upload.wikimedia.org/wikipedia/en/d/d8/RuleofRose.jpg'),
+    ('Cory In The House', 2008, 'Disney Interactive Studios', 40.00, 8.00, 'Adventure Stealth', false, 'https://upload.wikimedia.org/wikipedia/en/0/03/Cory_in_the_House_Nintendo_DS_Box_Art.jpg');
